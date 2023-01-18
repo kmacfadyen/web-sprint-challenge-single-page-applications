b3db338c1ef4f1892fd27e2e6ec15109f4d551f7
Kevin MacFadyen <kmacfadyen3@gmail.com>
2023-01-18 02:01:33 -0500

    checkpoint 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React, { useState, useEffect } from "react";
-// import axios from 'axios';
+import axios from 'axios';
 import Form from "./components/Form";
 // import * as yup from 'yup';
+import { Route, Link, Switch } from 'react-router-dom';
 
 const App = () => {
+
+const pizzaPost = pizzaOrder => {
+  axios.post('https://reqres.in/api/orders', pizzaOrder)
+  .then(res => {
+    setPizza(res.data);
+  })
+  .catch(err => console.error(err))
+}
+
+
+
+
   return (
     <div className='App'>
       <h1>Lambda Eats</h1>
       <p>Build Your Own Pizza!</p>
 
-      <Form
-      />
+      <Route exact path='/'>
+        
+      </Route>
+      
+    <Form
+
+
+    />
       
   </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Form = (props) => {
 
-    const handleChange = event => {
+    const onChange = event => {
         // const { name, value } = event.target;
         const { name, value, type, checked } = event.target;
         // const { type, checked } = event.target;
@@ -18,34 +18,112 @@ const Form = (props) => {
     }
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <p>Choice of Sauce</p>
-        <label>Original Red
-        <input
+    <Form id='pizza-form' onSubmit={handleSubmit}>
+
+        <div className='size-dropdown'>
+            <label><p>Choice of Size</p></label>
+            <select
+                name='size'
+                id='size-dropdown'
+                onChange={onChange}
+                value={values.size}
+            >
+              <option value='Small'>Small</option>
+              <option value='Medium'>Medium</option>
+              <option value='Large'>Large</option>  
+            </select>
+
+        </div>
+        <div className='pizza-sauce'>
+            <p>Choice of Sauce</p>
+            <label>Original Red
+            <input
         //   placeholder='Select'
-          type='radio'
-          name='size'
-          value='original red'
-          handleChange={handleChange}
+                type='radio'
+                name='sauce'
+                value='original red'
+                onChange={onChange}
         //   checked={values.}
-        />
-        </label>
-        <label>Garlic Ranch
-        <input
-          type='radio'
-          name='size'
-          value='garlic ranch'
-          handleChange={handleChange}
-        />
-        </label>
-        <label>BBQ Sauce
-        <input
-          type='radio'
-          name='size'
-          value='bbq sauce'
-          handleChange={handleChange}
-        />
-        </label>
+            />
+            </label>
+            <label>Garlic Ranch
+            <input
+                type='radio'
+                name='sauce'
+                value='garlic ranch'
+                onChange={onChange}
+            />
+            </label>
+            <label>BBQ Sauce
+            <input
+                type='radio'
+                name='sauce'
+                value='bbq sauce'
+                onChange={onChange}
+            />
+            </label>
+            <label>Spinach Alfredo
+            <input
+                type='radio'
+                name='sauce'
+                value='spinach alfredo'
+                onChange={onChange}
+            />
+            </label>
+        </div>
+
+        <div className='pizza-toppings'>
+
+        </div>
+
+        <div className='pizza-toppings'>
+            <p>Add Toppings</p>
+            <label>Pepperoni
+            <input
+              type='checkbox'
+              name='pepperoni'
+              checked={values.pepperoni}
+              onChange={onChange}
+            />
+            </label>
+            <label>Sausage
+            <input
+              type='checkbox'
+              name='sausage'
+              checked={values.sausage}
+              onChange={onChange}
+            />
+            </label>
+            <label>Onions
+            <input
+              type='checkbox'
+              name='onions'
+              checked={values.onions}
+              onChange={onChange}
+            />
+            </label>
+            <label>Extra Cheese
+            <input
+              type='checkbox'
+              name='cheese'
+              checked={values.cheese}
+              onChange={onChange}
+            />
+            </label>
+
+        </div>
+
+        <div className='special-text'>
+            <p>Special Instructions</p>
+            <label>
+              <input
+                type='text'
+                name='special'
+                value={values.special}
+                onChange={onChange}
+              />
+            </label>
+        </div>
 
         <button className='btn btn-default' type='submit'>
             Add to Order
